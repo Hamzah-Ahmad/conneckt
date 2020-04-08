4f439b1e32b6a6e9e32618a0e0726844ce89e6b3
Hamzah Ahmad <ahmadhamzah500@gmail.com>
2020-04-07 20:13:11 -0700

    Added some styling

--- client/src/components/layout/HomePage.js
+++ client/src/components/layout/HomePage.js
@@ -8,11 +8,31 @@ import AppNavbar from "./AppNavbar";
 import PostComponent from "../post/PostComponent";
 import PostTextBox from "./PostTextBox";
 import Container from "@material-ui/core/Container";
-import axios from "axios";
-import ImageUpload from "./ImageUpload";
-// import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles((theme) => ({
+  homePage: {
+    display: "flex",
+  },
+  mainSection: {},
+  sideBar: {
+    background: "#fafafa",
+    height: "100vh",
+    width: "18vw",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  userImage: {
+    marginTop: 30,
+    height: 160,
+    width: 160,
+    borderRadius: 100,
+  },
+}));
 const HomePage = (props) => {
+  const classes = useStyles();
+
   React.useEffect(() => {
     props.getPosts();
     // eslint-disable-next-line
@@ -20,42 +40,43 @@ const HomePage = (props) => {
   const posts = props.posts.posts;
   const { isAuthenticated } = props.auth;
 
-  const uploadImage = async (e) => {
-    const files = e.target.files[0];
-    const formData = new FormData();
-    formData.append("upload_preset", "connekt");
-    formData.append("file", files);
-    const data = await axios.post(
-      "https://api.cloudinary.com/v1_1/dbqbqfiyn/image/upload",
-      formData
-    );
-    props.changeImage(data, props.loadUser);
-    // console.log(imageUrl);
-  };
+  // const uploadImage = async (e) => {
+  //   const files = e.target.files[0];
+  //   const formData = new FormData();
+  //   formData.append("upload_preset", "connekt");
+  //   formData.append("file", files);
+  //   const data = await axios.post(
+  //     "https://api.cloudinary.com/v1_1/dbqbqfiyn/image/upload",
+  //     formData
+  //   );
+  //   props.changeImage(data, props.loadUser);
+  //   // console.log(imageUrl);
+  // };
   return (
     <div>
       <AppNavbar />
       {/* <input type="file" name="file" onChange={uploadImage} /> */}
-      <ImageUpload />
-      <Container maxWidth="sm">
-        {props.location.state && !isAuthenticated ? (
-          <p color="danger">{props.location.state.msg}</p>
-        ) : null}
-
-        <div>
-          <img
-            src={props.auth.user.image}
-            style={{ width: "30px", height: "30px" }}
-          />
-          <PostTextBox />
-          {posts &&
-            posts.map((post) => (
-              <div key={post._id}>
-                <PostComponent post={post} />
-              </div>
-            ))}
+      {/* <ImageUpload /> */}
+      <div className={classes.homePage}>
+        <div className={classes.sideBar}>
+          <img src={props.auth.user.image} className={classes.userImage} />
         </div>
-      </Container>
+        <Container maxWidth="sm" className={classes.mainSection}>
+          {props.location.state && !isAuthenticated ? (
+            <p color="danger">{props.location.state.msg}</p>
+          ) : null}
+
+          <div>
+            <PostTextBox />
+            {posts &&
+              posts.map((post) => (
+                <div key={post._id}>
+                  <PostComponent post={post} />
+                </div>
+              ))}
+          </div>
+        </Container>
+      </div>
     </div>
   );
 };
