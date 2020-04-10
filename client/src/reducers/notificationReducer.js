c8772af91a74a1b81ffd24b4b81960770c3f55ea
Hamzah Ahmad <ahmadhamzah500@gmail.com>
2020-04-09 19:16:58 -0700

    Reversed notifications recieved

--- client/src/reducers/notificationReducer.js
+++ client/src/reducers/notificationReducer.js
@@ -1,16 +1,16 @@
 import { GET__NOTIFICATIONS, DELETE_NOTIFICATION } from "../actions/types";
 const initialState = {
-  notifications: []
+  notifications: [],
 };
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
   switch (action.type) {
     case GET__NOTIFICATIONS:
       return {
-        notifications: action.payload
+        notifications: action.payload.reverse(),
       };
     case DELETE_NOTIFICATION:
       return {
-        notifications: action.payload
+        notifications: action.payload,
       };
     default:
       return state;
